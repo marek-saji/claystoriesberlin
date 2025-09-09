1887d8a587037ae4148cfd12bc5f1f3fadd1114b
Marek ‘saji’ Augustynowicz <marek.aug@gmail.com>
2025-09-09 13:32:25 +0200

    fixup! Detect if it’s preview

--- src/_data/site.mjs
+++ src/_data/site.mjs
@@ -1,7 +1,7 @@
 const isPreview = process.env.CF_PAGES_BRANCH !== 'main'
 
 const site = {
-  url: (!isPreview && process.env.CF_PAGES_URL) || 'https://claystoriesberlin.com',
+  url: (isPreview && process.env.CF_PAGES_URL) || 'https://claystoriesberlin.com',
   name: 'Clay Stories Berlin',
   author: 'Clay Stories Berlin',
   robots: isPreview ? 'noindex, nofollow' : 'index, follow',
