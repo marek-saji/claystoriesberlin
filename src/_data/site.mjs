892521b9d11bbc91b85621a4de720ac6bbb0d79f
Marek ‘saji’ Augustynowicz <marek.aug@gmail.com>
2025-11-15 21:53:05 +0100

    Business address 🏙️

--- src/_data/site.mjs
+++ src/_data/site.mjs
@@ -1,16 +1,31 @@
 const isPreview = process.env.CF_PAGES_BRANCH !== 'main'
 
-const name = 'Clay Stories Berlin'
+const name = 'Clay Stories'
+const siteName = `${name} Berlin`
 const companyName = 'Selter & Margoszczyn GbR'
 
 const site = {
   url: (isPreview && process.env.CF_PAGES_URL) || 'https://claystoriesberlin.com',
-  name,
+  robots: isPreview ? 'noindex, nofollow' : 'index, follow',
+  name: siteName,
   author: {
-    name: `${name} (${companyName})`,
+    name,
     email: 'claystories.berlin@gmail.com',
   },
-  robots: isPreview ? 'noindex, nofollow' : 'index, follow',
+  business: {
+    name: companyName,
+    address: {
+      name: `${companyName} ℅ ${name}`,
+      streetAddress: 'Kollwitzstraße 82',
+      postalCode: '10435',
+      locality: 'Berlin',
+      countryName: 'Germany',
+      coordinates: {
+        latitude: 52.5376384,
+        longitude: 13.4173513,
+      },
+    },
+  },
 }
 
 export default site
